cb5a18551803c90d98a94d146577ef318a4d71b9
Raghu Betina <rbetina@github.com>
2015-01-30 13:52:52 -0600

    Completes movies#destroy

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,17 @@
 class MoviesController < ApplicationController
 
   def index
-    @movies = Movie.order("title ASC")
+    @movies = Movie.all
   end
 
   def show
     @movie = Movie.find_by({ :id => params["id"] })
+  end
+
+  def destroy
+    @movie = Movie.find_by({ :id => params["id"] })
+
+    @movie.destroy
   end
 
 end
