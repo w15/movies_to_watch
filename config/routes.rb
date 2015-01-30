094490805835c85353771c5bec830d21d72cf6f0
Raghu Betina <rbetina@github.com>
2015-01-30 14:26:18 -0600

    Adds route for new movie form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get("/movies", { :controller => "movies", :action => "index" })
 
+  get("/movies/new", { :controller => "movies", :action => "new_form" })
+
   get("/movies/:id", { :controller => "movies", :action => "show" })
 
   get("/delete_movie/:id", { :controller => "movies", :action => "destroy" })
